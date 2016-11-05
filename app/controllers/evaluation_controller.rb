2cc41d49619f574d7a5900118496670e14ff0de4
Alireza <lrzmjd18@gmail.com>
2016-11-05 15:22:10 -0500

    added the course names to the search filter for evaluations table

--- app/controllers/evaluation_controller.rb
+++ app/controllers/evaluation_controller.rb
@@ -58,7 +58,13 @@ class EvaluationController < ApplicationController
     if can? :read, :all
       @terms = Evaluation.pluck(:term).uniq.sort.reverse
       @instructor_names = Instructor.pluck(:name).uniq.sort
-      @course_names = CourseName.pluck(:subject_course).uniq.sort
+      @course_names = CourseName.pluck(:subject_course, :name).uniq.sort
+
+      merge_subj_name = lambda { |subj, name| return name.nil? ? subj : subj + " " + name }
+
+      @course_names.map! { |crs|
+        merge_subj_name.call(crs[0], crs[1])
+      }
 
       term = params[:id] || Evaluation.no_missing_data.pluck(:term).uniq.sort.reverse.first
       instructor_name = params[:instructor_name]
